43df2dd33ed47ba3143efd27df91cf8b8d310523
Michele Bellin <softarea@softarea.it>
2023-05-08 12:22:52 +0200

    Aggiungere i file di progetto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace iOSReleaseLocalizationIssue;
+
+public partial class MainPage : ContentPage
+{
+	System.Resources.ResourceManager resourceManager = new System.Resources.ResourceManager("iOSReleaseLocalizationIssue.Resources.MyLocalization", typeof(MainPage).Assembly);
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnSetEngCulture(object sender, EventArgs e)
+	{
+		Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+        CultureStringLabel.Text = resourceManager.GetString("MyString");
+		CurrentCultureLabel.Text = "Current Culture: " + Thread.CurrentThread.CurrentCulture;
+        CurrentUICultureLabel.Text = "Current UI Culture: " + Thread.CurrentThread.CurrentUICulture;
+
+    }
+	private void OnSetDeCulture(object sender, EventArgs e)
+	{
+		Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture = new CultureInfo("de");
+        CultureStringLabel.Text = resourceManager.GetString("MyString");
+        CurrentCultureLabel.Text = "Current Culture: " + Thread.CurrentThread.CurrentCulture;
+        CurrentUICultureLabel.Text = "Current UI Culture: " + Thread.CurrentThread.CurrentUICulture;
+    }
+    private void OnSetEsCulture(object sender, EventArgs e)
+    {
+        Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture = new CultureInfo("es");
+        CultureStringLabel.Text = resourceManager.GetString("MyString");
+        CurrentCultureLabel.Text = "Current Culture: " + Thread.CurrentThread.CurrentCulture;
+        CurrentUICultureLabel.Text = "Current UI Culture: " + Thread.CurrentThread.CurrentUICulture;
+    }
+}
+
